8179a424415cf648f96c34f1ff3c0d3c54e38c9f
010011101 <13511059968@163.com>
2018-03-26 16:45:05 +0800

    a

---
+++ src/test1/Test001.java
@@ -0,0 +1,13 @@
+package test1;
+/**
+ * @description TODO
+ * @modifyTime 
+ * @version 1.0
+ */
+public class Test001 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
